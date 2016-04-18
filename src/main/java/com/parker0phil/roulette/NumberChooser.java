add30388c57c56837c9e6c47224714751bd43abd
Phil Parker <parker0phil.github@cadenceone.com>
2016-04-18 14:05:09 +0100

    added number chooser

--- src/main/java/com/parker0phil/roulette/NumberChooser.java
+++ src/main/java/com/parker0phil/roulette/NumberChooser.java
@@ -1,4 +1,17 @@
 package com.parker0phil.roulette;
 
+import java.util.Random;
+
 public class NumberChooser {
+
+    private final Random source;
+
+    public NumberChooser(Random source){
+        this.source = source;
+    }
+
+    public int choose(){
+        //the next pseudorandom, uniformly distributed {@code int} value between zero (inclusive)
+        return source.nextInt(36);
+    }
 }
--- src/test/groovy/com/parker0phil/roulette/NumberChooserSpec.groovy
+++ src/test/groovy/com/parker0phil/roulette/NumberChooserSpec.groovy
@@ -4,4 +4,18 @@ import spock.lang.Specification
 
 class NumberChooserSpec extends Specification {
 
+    def "chooses number from random data source"() {
+
+        given:
+        def random = Mock(Random)
+        def chooser = new NumberChooser(random)
+        def anyNumber = 5;
+
+        when:
+        def chosen = chooser.choose()
+
+        then:
+        1 * random.nextInt(36) >> anyNumber
+        chosen == anyNumber
+    }
 }
